58eff3794a66c28c6a3a9588b2535537ff031a7f
Cezar Gabriel Heinen Zaleski <cezar.zaleski@gmail.com>
2021-04-30 20:33:05 -0300

    feat: ensure SignUpController returns 400  error if no email is provided

--- src/presentation/controllers/signup.spec.ts
+++ src/presentation/controllers/signup.spec.ts
@@ -14,4 +14,18 @@ describe('', () => {
     expect(httpResponse.statusCode).toBe(400)
     expect(httpResponse.body).toEqual(new Error('Missing param: name'))
   })
+
+  test('Shout return 400 if no email is provide', () => {
+    const sut = new SignupController()
+    const httpResquest = {
+      body: {
+        name: 'any_name',
+        password: 'any_password',
+        passwordConfirmation: 'any_password'
+      }
+    }
+    const httpResponse = sut.handle(httpResquest)
+    expect(httpResponse.statusCode).toBe(400)
+    expect(httpResponse.body).toEqual(new Error('Missing param: email'))
+  })
 })
