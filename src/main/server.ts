6e7bb2420200cbd9c4d97ff5f791631cf95abc9c
Cezar Gabriel Heinen Zaleski <cezar.zaleski@gmail.com>
2021-05-02 00:50:21 -0300

    feat: connect to mongodb before app listen

--- src/main/server.ts
+++ src/main/server.ts
@@ -1,4 +1,10 @@
-import express from 'express'
 
-const app = express()
-app.listen(5050, () => console.log('Server running at http://localhost:5050'))
+import { MongoHelper } from '../infra/db/mongodb/helpers/mongo-helper'
+import env from '../main/config/env'
+
+MongoHelper.connect(env.mongoUrl)
+  .then(async () => {
+    const app = (await import('./config/app')).default
+    app.listen(env.port, () => console.log(`Server running at http://localhost:${env.port}`))
+  })
+  .catch(console.error)
